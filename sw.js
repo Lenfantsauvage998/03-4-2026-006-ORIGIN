57651de812ff539dc92b48616b8e75d1e1d25cd2
Lenfantsauvage998 <144186816+Lenfantsauvage998@users.noreply.github.com>
2026-03-24 09:39:56 -0500

    Update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump version whenever static assets change
-const CACHE = 'dfg-v14';
+const CACHE = 'dfg-v16';
 
 // Derive base path from SW location so any GitHub Pages repo name works
 const BASE = self.location.pathname.replace(/\/sw\.js$/, '');
