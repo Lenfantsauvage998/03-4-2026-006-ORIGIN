60cf6a5668b7114311a66d394807df82c2b4b272
Lenfantsauvage998 <144186816+Lenfantsauvage998@users.noreply.github.com>
2026-03-29 21:24:36 -0500

    Interface_Update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump version whenever static assets change
-const CACHE = 'dfg-v20';
+const CACHE = 'dfg-v21';
 
 // Derive base path from SW location so any GitHub Pages repo name works
 const BASE = self.location.pathname.replace(/\/sw\.js$/, '');
